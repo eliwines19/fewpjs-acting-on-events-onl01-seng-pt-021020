3afddfb0964255690a1d1370de837e0b99e83c84
Elijah wines <elijahwines84@yahoo.com>
2020-08-10 20:31:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,5 +17,14 @@ document.addEventListener("keydown", function(e) {
 })
 
 function moveDodgerRight() {
-  let leftNumbers = dodger.style.left.replace("px", "")
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10)
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`
+  }
 }
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
