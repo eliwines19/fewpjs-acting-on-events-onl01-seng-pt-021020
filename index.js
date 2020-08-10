3ba24cde3ecad1e85075fb40f488061de556c089
Elijah wines <elijahwines84@yahoo.com>
2020-08-10 20:31:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+
 function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace("px", "")
   let left = parseInt(leftNumbers, 10)
@@ -13,3 +15,7 @@ document.addEventListener("keydown", function(e) {
     moveDodgerLeft();
   }
 })
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "")
+}
